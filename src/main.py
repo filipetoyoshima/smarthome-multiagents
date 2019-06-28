4c4856614db949ba6d6b41dc89a6585fb12681c7
Filipe Toyoshima <filipetoyoshima@gmail.com>
2019-06-28 10:59:40 -0300

    created basic structure to start main.py

---
+++ src/main.py
@@ -0,0 +1,27 @@
+from osbrain import run_agent
+from osbrain import run_nameserver
+
+"""
+TODO: create handlers
+
+def switch(agent, message):
+    agent.log_info('received message')
+"""
+
+if __name__ == '__main__':
+
+    # System Deployment
+    """ TODO: instantiate all agents
+    sensorA = run_agent('sensorA')
+    lampA = run_agent('lampA')
+    """
+
+    # System SetUp
+    """ TODO: make the needed connections
+    addrSensor = sensorA.bind('PUB', alias='sensorAAddr')
+    lampA.connect (addrSensor, handler=switch)
+    """
+
+    # Send message
+    """ TODO: monitor things in smart home
+    """
