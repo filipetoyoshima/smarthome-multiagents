76c955b2aa6086cfd64adfebeb9b4099cc8c0074
Filipe Toyoshima <filipetoyoshima@gmail.com>
2019-06-28 23:32:42 -0300

    added basic setup

--- src/main.py
+++ src/main.py
@@ -15,12 +15,47 @@ if __name__ == '__main__':
     sensorA = run_agent('sensorA')
     lampA = run_agent('lampA')
     """
+    ns = run_nameserver()
+    temperatureSensor = run_agent('tempertature')
+    luminousSensor = run_agent('luminous')
+    presenceSensor = run_agent('presence')
+    proximitySensor = run_agent('proximity')
+    eletricController = run_agent('eletric')
+    arConditionerController = run_agent('arConditioner')
+    lampController = run_agent('lamp')
+    doorController = run_agent('door')
+    person = run_agent('person')
+    environment = run_agent('environment')
+
 
     # System SetUp
-    """ TODO: make the needed connections
-    addrSensor = sensorA.bind('PUB', alias='sensorAAddr')
-    lampA.connect (addrSensor, handler=switch)
-    """
+    # Two sensors that will observe the person in home
+    personAddr = person.bind('PUB', alias='person')
+    presenceSensor.connect(personAddr, handler=checkLocation)
+    proximitySensor.connect(personAddr, handler=checkDistance)
+    
+    # Two sensors that will observe enviroment details
+    environAddr = environment.bind('PUB', alias='environment')
+    temperatureSensor.connect(environAddr, handler=setTemperature)
+    luminousSensor.connect(environAddr, handler=setLight)
+
+    # Temperature sensor will send data to the arConditioner
+    temperatureAddr = temperatureSensor('PUB', alias='temperature')
+    arConditionerController.connect(temperatureAddr, handler=getTemperature)
+    
+    # Light sensor will send data to the lamps
+    luminousAddr = luminousSensor('PUB', alias='light')
+    lampController.connect(luminousAddr, handler=receiveLight)
+
+    # Presence sensor will send data to the arConditioner and to the lamps
+    presenceAddr = presenceSensor('PUB', alias='presence')
+    arConditionerController.connect(presenceAddr, handler=seekPresence)
+    lampController.connect(presenceAddr, handler=setLight)
+
+    # Proximity sensor will send data to the doors controlers
+    proxAddr = proximitySensor('PUB', alias='proximity')
+    doorController.connect(proxAddr, handler=setOpen)
+    
 
     # Send message
     """ TODO: monitor things in smart home
