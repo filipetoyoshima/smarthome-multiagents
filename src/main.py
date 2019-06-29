106ac347af495963d08cdb83017d7d66f226d1ad
Filipe Toyoshima <filipetoyoshima@gmail.com>
2019-06-29 00:18:29 -0300

    added some methods

--- src/main.py
+++ src/main.py
@@ -8,9 +8,21 @@ def switch(agent, message):
     agent.log_info('received message')
 """
 
+def getTemperature(self, message):
+    if message > 293:
+        self.isHeat = True
+    else:
+        self.isHeat = False
+
+
+def seePresence(self, message):
+    self.presence = message
+
+
 if __name__ == '__main__':
 
     ### System Deployment ###
+    ### This will define the agents on program
     
     ns = run_nameserver()
     
@@ -25,14 +37,14 @@ if __name__ == '__main__':
     environment = run_agent(
         name = 'environment',
         attributes = dict(
-            light = 1000, # lx, see https://en.wikipedia.org/wiki/Lux#Illuminance
-            temperature = 293 # °k
+            light = 1000, # the unit is lx, see https://en.wikipedia.org/wiki/Lux#Illuminance
+            temperature = 293 # the unit is °k
         )
     )
     
     # Sensors
     temperatureSensor = run_agent('tempertature')
-    luminousSensor = run_agent('luminous')
+    luminositySensor = run_agent('luminous')
     presenceSensor = run_agent('presence')
     proximitySensor = run_agent('proximity')
     
@@ -46,13 +58,17 @@ if __name__ == '__main__':
     arConditionerController = run_agent(
         'arConditioner',
         attributes = dict (
+            presence = False,
+            isHeat = False,
             power = False,
-            airTemperature = 289,
+            airTemperature = 289
         )
     )
     lampController = run_agent(
         'lamp',
         attributes = dict (
+            presence = False,
+            isNeeded = False,
             power = False
         )
     )
@@ -65,6 +81,8 @@ if __name__ == '__main__':
 
 
     ### System Setup ###
+    ### This will connect agents that must send/receive data
+
     # Two sensors that will observe the person in home
     personAddr = person.bind('PUB', alias='person')
     presenceSensor.connect(personAddr, handler=checkLocation)
@@ -73,20 +91,20 @@ if __name__ == '__main__':
     # Two sensors that will observe enviroment details
     environAddr = environment.bind('PUB', alias='environment')
     temperatureSensor.connect(environAddr, handler=setTemperature)
-    luminousSensor.connect(environAddr, handler=setLight)
+    luminositySensor.connect(environAddr, handler=setLight)
 
     # Temperature sensor will send data to the arConditioner
     temperatureAddr = temperatureSensor('PUB', alias='temperature')
     arConditionerController.connect(temperatureAddr, handler=getTemperature)
     
     # Light sensor will send data to the lamps
-    luminousAddr = luminousSensor('PUB', alias='light')
+    luminousAddr = luminositySensor('PUB', alias='light')
     lampController.connect(luminousAddr, handler=receiveLight)
 
     # Presence sensor will send data to the arConditioner and to the lamps
     presenceAddr = presenceSensor('PUB', alias='presence')
-    arConditionerController.connect(presenceAddr, handler=seekPresence)
-    lampController.connect(presenceAddr, handler=setLight)
+    arConditionerController.connect(presenceAddr, handler=seePresence)
+    lampController.connect(presenceAddr, handler=seePresence)
 
     # Proximity sensor will send data to the doors controlers
     proxAddr = proximitySensor('PUB', alias='proximity')
